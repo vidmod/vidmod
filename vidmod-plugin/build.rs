5abd05a9ba37aa99bc9dea79240569f56424ef51
chordtoll <git@chordtoll.com>
2022-05-02 22:07:19 -0700

    Migrate to dynamically linked plugins

--- vidmod-plugin/build.rs
+++ vidmod-plugin/build.rs
@@ -1,83 +1,17 @@
-use std::{
-    env,
-    fs::{read_dir, File},
-    io::{BufRead, BufReader, Write},
-    path::Path,
-};
-
-use regex::Regex;
+use std::{env, fs::File, io::Write, path::Path};
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("plugins.rs");
+
+    let dest_path = Path::new(&out_dir).join("libdir.rs");
     let mut test_file = File::create(&dest_path).unwrap();
-
     test_file
         .write_all(
-            b"lazy_static! {
-        pub static ref PLUGINS: BTreeMap<String, Plugin> = {
-            let mut res = BTreeMap::new();\n",
-        )
-        .unwrap();
-
-    let plugin_directories = read_dir("../vidmod-plugins/").unwrap();
-
-    for dir in plugin_directories {
-        let dir = dir.unwrap();
-        if dir.file_name().to_str().unwrap().bytes().next().unwrap() == b'.' {
-            continue;
-        }
-
-        let crate_name = dir.file_name().as_os_str().to_str().unwrap().to_owned();
-        let crate_name_clean = crate_name.replace('-', "_");
-
-        let lib = File::open(dir.path().join("src").join("lib.rs")).unwrap();
-        for line in BufReader::new(lib).lines() {
-            let r = Regex::new(r"^pub mod ([^ ]+);$").unwrap();
-            if let Some(cap) = r.captures(&line.unwrap()) {
-                let module_name = cap.get(1).unwrap().as_str();
-                if module_name == "plugin" {
-                    continue;
-                }
-                let module_file = dir.path().join("src").join(format!("{}.rs", module_name));
-                let module = File::open(module_file).unwrap();
-                let mut lines = BufReader::new(module).lines();
-                while let Some(Ok(line)) = lines.next() {
-                    if line == "#[node_decl]" {
-                        let line = lines.next().unwrap().unwrap();
-                        let r = Regex::new(r"^pub struct ([^ ]+) \{$").unwrap();
-                        let plugin_struct = r.captures(&line).unwrap().get(1).unwrap().as_str();
-
-                        test_file
-                            .write_all(
-                                format!(
-                                    "\t\t\tlet plugin = Plugin {{ make_node: |params| Node(Box::new({}::{}::{}::new(params)))}}; \n",
-                                    crate_name_clean, module_name, plugin_struct
-                                )
-                                .as_bytes(),
-                            )
-                            .unwrap();
-
-                        test_file
-                            .write_all(
-                                format!(
-                                    "\t\t\tres.insert(\"{}::{}\".to_owned(),plugin);\n",
-                                    crate_name, plugin_struct
-                                )
-                                .as_bytes(),
-                            )
-                            .unwrap();
-                    }
-                }
-            }
-        }
-    }
-
-    test_file
-        .write_all(
-            b"\t\t\tres
-\t\t};
-}",
+            format!(
+                "static OUT_DIR : &'static str = \"{}/../target\";",
+                env!("CARGO_MANIFEST_DIR")
+            )
+            .as_bytes(),
         )
         .unwrap();
 
--- vidmod-plugin/src/lib.rs
+++ vidmod-plugin/src/lib.rs
@@ -3,10 +3,46 @@ extern crate lazy_static;
 
 use std::collections::BTreeMap;
 
+use glob::glob;
 use vidmod_node::Node;
+
+pub type PluginRegSymbol<'a> = libloading::Symbol<
+    'a,
+    extern "C" fn() -> Vec<(String, fn(params: BTreeMap<String, String>) -> Node)>,
+>;
 
 pub struct Plugin {
     pub make_node: fn(params: BTreeMap<String, String>) -> Node,
 }
 
-include!(concat!(env!("OUT_DIR"), "/plugins.rs"));
+lazy_static! {
+    pub static ref PLUGIN_LIBRARIES: BTreeMap<String, libloading::Library> = {
+        let mut res = BTreeMap::new();
+        println!("Searching for plugins in {}/debug/", OUT_DIR);
+        for i in glob(&format!("{}/release/libvidmod_plugins_*.so", OUT_DIR)).unwrap() {
+            let lib = unsafe { libloading::Library::new(i.unwrap()).unwrap() };
+            let plugin_name: libloading::Symbol<extern "C" fn() -> String> =
+                unsafe { lib.get(b"plugin_name").unwrap() };
+            res.insert(plugin_name(), lib);
+        }
+        res
+    };
+}
+
+lazy_static! {
+    pub static ref PLUGINS: BTreeMap<String, Plugin> = {
+        let mut res = BTreeMap::new();
+        for (plugin_name, lib) in PLUGIN_LIBRARIES.iter() {
+            let register_plugin: PluginRegSymbol = unsafe { lib.get(b"register_plugin").unwrap() };
+            for (node_name, make_node) in register_plugin() {
+                res.insert(
+                    format!("{}::{}", plugin_name, node_name),
+                    Plugin { make_node },
+                );
+            }
+        }
+        res
+    };
+}
+
+include!(concat!(env!("OUT_DIR"), "/libdir.rs"));
